f815bfd1a78094058f96184506e3bb3108ad5cdd
alexvorxx <72611580+alexvorxx@users.noreply.github.com>
2024-04-29 14:03:55 +0300

    Update zink_screen.c
    Comment driParseConfigFiles

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -3345,8 +3345,8 @@ zink_internal_create_screen(const struct pipe_screen_config *config, int64_t dev
 
    screen->instance_info.loader_version = zink_get_loader_version(screen);
    if (config) {
-      driParseConfigFiles(config->options, config->options_info, 0, "zink",
-                          NULL, NULL, NULL, 0, NULL, 0);
+      //driParseConfigFiles(config->options, config->options_info, 0, "zink",
+                          //NULL, NULL, NULL, 0, NULL, 0);
       //screen->driconf.dual_color_blend_by_location = driQueryOptionb(config->options, "dual_color_blend_by_location");
       //screen->driconf.inline_uniforms = driQueryOptionb(config->options, "radeonsi_inline_uniforms");
       //screen->driconf.emulate_point_smooth = driQueryOptionb(config->options, "zink_emulate_point_smooth");
